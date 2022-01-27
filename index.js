e9e1dc66072726c77408a0107179c0dc60fc2932
John Merz <jmerz826@gmail.com>
2022-01-26 20:54:50 -0500

    exercise 7 finished

--- index.js
+++ index.js
@@ -209,6 +209,7 @@ class Car {
  */
 function isEvenNumberAsync(number) {
   // ✨ implement
+  return (number % 2 === 0) ? true : false;
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -133,9 +133,14 @@ describe('[Exercise 6] Car', () => {
     expect(focus.tank).toBe(20)
   })
 })
-/*
+
 describe('[Exercise 7] isEvenNumberAsync', () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  // test('[20] resolves false if passed an odd number', () => {})
+  test('[19] resolves true if passed an even number', async () => {
+    const result = await utils.isEvenNumberAsync(4)
+    expect(result).toBe(true)
+  })
+  test('[20] resolves false if passed an odd number', async () => {
+    const result = await utils.isEvenNumberAsync(5)
+    expect(result).toBe(false)
+  })
 })
-*/
